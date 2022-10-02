df24d8cdcea89fb235e6cd39220c84ac2899084f
DavidWisdom <88563609+DavidWisdom@users.noreply.github.com>
2022-10-02 10:27:21 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,4 +1,15 @@
 //
 // Created by DavidWisdom on 2022/10/2.
 //
+#include <iostream>
+#include "src/stack.h"
+#include "src/queue.h"
+using namespace std;
+int main() {
+    for (int i = 0; i < 100; ++i) {
+        MyQueue::push(i);
+        cout << MyQueue::front() << endl;
+        MyQueue::pop();
+    }
+}
 
